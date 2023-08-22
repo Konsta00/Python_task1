9a17c10508c6357ae1f3c0effc452f519f7348c6
Konsta00 <56935705+Konsta00@users.noreply.github.com>
2023-08-22 13:39:57 +0300

    Create arpoja.py

---
+++ arpoja.py
@@ -0,0 +1,14 @@
+import math
+import random
+
+nro1 = random.randint(0, 9)
+nro2 = random.randint(0, 9)
+nro3 = random.randint(0, 9)
+print(nro1, nro2, nro3)
+
+nro4 = random.randint(1, 6)
+nro5 = random.randint(1, 6)
+nro6 = random.randint(1, 6)
+nro7 = random.randint(1, 6)
+print(nro4, nro5, nro6, nro7)
+
