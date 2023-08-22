c9ce7756b31c2744a2c299504313990d7cb08e77
Konsta00 <56935705+Konsta00@users.noreply.github.com>
2023-08-22 13:53:34 +0300

    Update arpoja.py

--- arpoja.py
+++ arpoja.py
@@ -4,11 +4,11 @@ import random
 nro1 = random.randint(0, 9)
 nro2 = random.randint(0, 9)
 nro3 = random.randint(0, 9)
-print(nro1, nro2, nro3)
+print(f"{nro1}{nro2}{nro3}")
 
 nro4 = random.randint(1, 6)
 nro5 = random.randint(1, 6)
 nro6 = random.randint(1, 6)
 nro7 = random.randint(1, 6)
-print(nro4, nro5, nro6, nro7)
+print(f"{nro4}{nro5}{nro6}{nro7}")
 
