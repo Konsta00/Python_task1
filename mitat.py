472a16609503bfd7d56bba0b664224bb90c7cbce
Konsta00 <56935705+Konsta00@users.noreply.github.com>
2023-08-22 13:11:27 +0300

    update

--- mitat.py
+++ mitat.py
@@ -1,14 +1,42 @@
 import math
 
-naula = 32 * 13,3
-
+luoti = 13.3
+naula = 32 * luoti
 leiviska = 20 * naula
 
-luoti = 13,3
+v = input('Anna leiviskät: ')
+try:
+    value = float(v)
+    l = float(v) * float(leiviska)
+except ValueError:
+    v = input('Anna leiviskät: ')
+    l = float(v) * float(leiviska)
 
 
-v1 = input('Anna leiviskät: ')
+v1 = input('Anna naulat: ')
+try:
+    value = float(v1)
+    n = float(v1) * float(naula)
+except ValueError:
+    v2 = input('Anna naulat: ')
+    n = float(v1) * float(naula)
 
-l = v1 * leiviska
 
-print('vastaus: ', l)
+v2 = input('Anna luodit: ')
+try:
+    value = float(v2)
+    b = float(v2) * float(luoti)
+except ValueError:
+    v2 = input('Anna luodit: ')
+    b = float(v2) * float(luoti)
+
+print('Massa nykyisten mittojen mukaan: ')
+
+
+g = float(l) + float(n) + float(b)
+print(g, 'grammaa')
+
+kg = g // 1000
+g2 = g % 1000
+
+print(kg, 'kg', g2, 'grammaa')
